26d29eac34f313a1ac8fafc6063d38ed7254d4ae
Nolann Fotso <njfotso@gmail.com>
2026-05-09 13:10:14 +0200

    feat: add teacher biographies

--- js/script.js
+++ js/script.js
@@ -478,12 +478,11 @@ document.addEventListener('DOMContentLoaded',()=> {
                         </div>
                         <div class="profile-cols">
                 <div>
-                    <h3>A subheader for some text</h3>
+                    <h3>Biographie</h3>
                     <p>${teacher.biographie}</p>
                 </div>
                 <div>
-                    <h3>A subheader for more text</h3>
-                    <p>Writing for websites is both simple and complex. On the one hand, all you need to do is say what you mean, in your words, in your voice. One the other, there are so many rules to consider! Are you thinking of keywords you should rank for? Are you including links in your text to additional information? Do those links refer back to your own website, which helps boost your SEO? Is what you've written easy to scan? There's a theory that people read in an F-shape pattern, and that this should influence how you structure content on your website. Lots of ins and outs—it's no wonder writers rule the world.</p>
+                    <h3>See Articles →</h3>
                 </div>
             </div>`
                     }
